3c0825e297cab29a1d45fc082df6c44a6a20065d
stephen njeri <stevenjeri33@gmail.com>
2021-07-01 21:57:03 +0300

    add modified js/scripts.js

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-function initMap(){
-    var location = {lat: 40.655030, lng: -97.881740};
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: location
-    });
-} 
